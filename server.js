e524db134d4f57c1aca2e5d124db743910195d41
Viraj Mehta <54108753+viraj-coder@users.noreply.github.com>
2021-09-21 20:44:38 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+const { v4: uuidv4 } = require("uuid");
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+});
+
+app.use("/peerjs", peerServer);
+var nodemailer=require("nodemailer");
+const { response } = require("express");
+const { info } = require("console");
+const transporter=nodemailer.createTransport({
+    port:587,
+    host:"smtp.gmail.com",
+    auth:{
+          user:"viramdo2202@gmail.com",
+          pass:"kpsokrcfjdmvcwzn"
+    },
+    secure:true
+})
+app.get("/", (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+    res.render("index", { roomId: req.params.room });
+});
+app.post("/send-mail",(req,res)=>{
+    const to=req.body.to;
+    const url=req.body.url;
+    const maildata={
+        from:"viramdo2202@gmail.com",
+        to:to,
+        subject:"join the video chat with me",
+        html:`<p> hey there, </p><p> come and join me for a video chat here-${url}</p>`
+    };
+    transporter.sendMail(maildata,(error,info)=>{
+        if(error){
+            return console.log(error)
+        }
+        res.status(200).send({messge:"invitation sent",message_id:info.messageId})
+    })
+})
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId, userName) => {
+        socket.join(roomId);
+        io.to(roomId).emit("user-connected", userId);
+        socket.on("message", (message) => {
+            io.to(roomId).emit("createMessage", message, userName);
+        });
+    });
+});
+
+server.listen(process.env.PORT || 3030);
